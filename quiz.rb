40c432dccbaaef7f165e74b8308101ee0539d3d6
Swamy G <swamy@goodguide.com>
2015-09-29 21:21:13 -0700

    Back to original idea

--- quiz.rb
+++ quiz.rb
@@ -47,7 +47,6 @@ class Quiz
     @questions = CSV.read("questions.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
     @unique_strand_ids = (@questions.map { |q| q[:strand_id] }).uniq
     @unique_standard_ids = (@questions.map { |q| q[:standard_id] }).uniq
-    @unique_question_ids = (@questions.map { |q| q[:question_id] }).uniq
 
     initialize_data
   end
@@ -62,6 +61,31 @@ class Quiz
     end
   end
 
+  # def choose_questions
+  #   chosen_questions = Array.new
+  #
+  #   # strand_count = @num_of_questions/@unique_strand_ids.length
+  #   # standard_count = @num_of_questions/@unique_standard_ids.length
+  #
+  #   (1..@num_of_questions).each do |n|
+  #     strand_index = n % @unique_strand_ids.length
+  #     standard_index = n % @unique_standard_ids.length
+  #     strand_id = @unique_strand_ids[strand_index]
+  #     standard_id = @unique_standard_ids[standard_index]
+  #
+  #     cq = @questions.select {|q| q.strand_id == strand_id && q.standard_id == standard_id }
+  #     binding.pry
+  #     cq.each do |chosen_question|
+  #       binding.pry
+  #       chosen_questions <<  chosen_question unless chosen_questions.any?{ |cq| cq.id == chosen_question.id }
+  #     end
+  #
+  #     # binding.pry
+  #   end
+  #
+  #   # binding.pry
+  # end
+
   def choose_questions
     chosen_questions = Array.new
 
@@ -69,8 +93,8 @@ class Quiz
     standard_count = @num_of_questions/@unique_standard_ids.length
 
     while chosen_questions.size < @num_of_questions do
-      @unique_strand_ids.cycle(strand_count) do |strand_id|
-        @unique_standard_ids.cycle(standard_count) do |standard_id|
+      @unique_standard_ids.cycle(standard_count) do |standard_id|
+        @unique_strand_ids.cycle(strand_count) do |strand_id|
           cq = @questions.select {|q| q.strand_id == strand_id && q.standard_id == standard_id }
 
           cq.each do |chosen_question|
